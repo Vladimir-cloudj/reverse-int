fb8937259e6d8ee8f59a24a0335a4d83e04f5fa7
Vladimir <wizart88@gmail.com>
2021-12-31 16:36:51 +0400

    its not optimal solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+	str = n + ''
+	// console.log(str[0]);
+	
+	if (!Number(str[0])) {
+		str = str.substr(1);
+	}
+	return str.split('').reverse().join('');
 }
